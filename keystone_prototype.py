08d5cde0e40d5e4f7465688f8eaec959d6d301bd
Matthew Yee <matthew.yee@netapp.com>
2014-12-03 13:04:48 -0800

    kinda working

--- keystone_prototype.py
+++ keystone_prototype.py
@@ -3,50 +3,16 @@ from flask import Flask, request, abort, jsonify
 app = Flask(__name__)
 
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
-
-
-@app.route('/whatup')
-def test():
-    return 'what\'s up doc'
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
-    if not request.json or not 'title' in request.json:
+@app.route('/s3/buckets', methods=['GET', 'POST'])
+def get_buckets():
+    if not request.json:
         abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
-
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
+    if request.method == 'GET':
+        return 'some get request'
+    if request.method == 'POST':
+        access_id = request.json['access_id']
+        access_secret = request.json['access_secret']
+        return jsonify(access_id) + access_secret, 201
 
 if __name__ == '__main__':
     app.run()
